7d717ee4658482bbb451b8c561853b04f4bdc129
Jerome Dockes <jerome@dockes.org>
2022-05-05 12:35:41 -0400

    iter neuroquery test

--- tests/test_fit_neuroquery.py
+++ tests/test_fit_neuroquery.py
@@ -87,6 +87,13 @@ def test_fit_neuroquery(extracted_data_dir, tfidf_dir):
     )
     model("term 3 term 45 term 10000")
     assert output_dir.joinpath("app.py").is_file()
+    # test re-running and overwriting previous model
+    output_dir.joinpath("info.json").unlink()
+    new_output_dir, code = _fit_neuroquery.fit_neuroquery(
+        tfidf_dir, extracted_data_dir, n_jobs=2
+    )
+    assert code == 0
+    assert new_output_dir == output_dir
 
 
 def test_does_not_rerun(tmp_path, monkeypatch):
