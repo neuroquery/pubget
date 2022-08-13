17ced1d76a396f8f634745c9c1991393f3fd17d2
Jerome Dockes <jerome@dockes.org>
2022-08-13 08:10:03 -0400

    add extracting links

---
+++ tests/test_links.py
@@ -0,0 +1,30 @@
+from lxml import etree
+import pytest
+
+from nqdc import _links
+
+
+@pytest.mark.parametrize("n_links", [0, 1, 5])
+def test_link_extractor(n_links):
+    xml_template = b"""<?xml version='1.0' encoding='ASCII'?>
+<article xmlns:xlink="http://www.w3.org/1999/xlink">
+    <front>
+        <article-meta>
+            <article-id pub-id-type="pmc">9057060</article-id>
+        </article-meta>
+    </front>
+    <body>
+    some links:
+    %s
+    </body>
+</article>
+    """
+    one_link = (
+        b'one link <ext-link xlink:href="http:example.com">'
+        b"http:example.com</ext-link>"
+    )
+    links_text = b"\n".join([one_link for i in range(n_links)])
+    xml = xml_template % links_text
+    document = etree.ElementTree(etree.XML(xml))
+    extracted = _links.LinkExtractor().extract(document)
+    assert extracted.shape == (n_links, 3)
