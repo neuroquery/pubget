617c1ff617c7548be688cd3c69333d3490406dc3
Jerome Dockes <jerome@dockes.org>
2022-09-02 16:54:40 -0400

    line too long in docstring

--- src/nqdc/_pipeline.py
+++ src/nqdc/_pipeline.py
@@ -21,9 +21,10 @@ _STEP_DESCRIPTION = "Run full nqdc pipeline."
 _FULL_PIPELINE_DESCRIPTION = (
     "Download and process full-text articles from PubMed Central "
     "for the given query or list of PMCIDs. Articles are downloaded and "
-    "stored in individual files. Then, their text and stereotactic coordinates "
-    "are extracted and stored in csv files. Finally, the text is "
-    "vectorized by computing word counts and TFIDF features."
+    "stored in individual files. Then, their text and "
+    "stereotactic coordinates are extracted and stored in csv files. "
+    "Finally, the text is vectorized by computing word counts "
+    "and TFIDF features."
 )
 
 
