fd11d63408e54bb016cb50494f2f5e2226db7c88
James Kent <jamesdkent21@gmail.com>
2024-11-11 10:28:26 -0600

    mess with newlines

--- src/pubget/_labelbuddy.py
+++ src/pubget/_labelbuddy.py
@@ -100,13 +100,13 @@ def _format_tables(doc_tables: pd.DataFrame, root_dir: Path) -> str:
             table_info["table_label"]
         table_str = f"## ID: {table_id}\n\n### Label: {table_label}\n\n"
         df = pd.read_csv(table_path, encoding="utf-8")
-        table_str += df.to_string() + "\n"
+        table_str += df.to_string() + "\n\n"
         table_caption = "None" if pd.isna(table_info["table_caption"]) else \
             table_info["table_caption"]
         table_foot = "None" if pd.isna(table_info["table_foot"]) else \
             table_info["table_foot"]
-        table_str += f"### Caption\n\n{table_caption}\n"
-        table_str += f"### Footer\n\n{table_foot}"
+        table_str += f"### Caption\n\n{table_caption}\n\n"
+        table_str += f"### Footer\n\n{table_foot}\n"
         table_texts.append(table_str)
 
     return "\n\n\n".join(table_texts)
